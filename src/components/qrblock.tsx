4c492f2829c44f93a6537b20b01a1ab4e85f01b8
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-05-11 20:00:45 +0300

    Сделал секцию qr кодов необходимо отцентрировать. Делаю Dropdown.

--- src/components/qrblock.tsx
+++ src/components/qrblock.tsx
@@ -4,7 +4,7 @@ export default function QrBlock() {
       <div className="container">
         <div className="grid grid-cols-1 ">
           <div className="ud-contact-content-wrapper">
-            <div className="ud-contact-title text-center">
+            <div className="ud-contact-title text-center px-4"> {/* Добавлен класс px-4 */}
               <div className="text-4xl font-semibold mb-4">Информация о городах-побратимах</div>
             </div>
             <div className="ud-contact-info-wrapper flex flex-wrap justify-center">
@@ -12,14 +12,14 @@ export default function QrBlock() {
               <div className="ud-single-info w-full md:w-1/2 flex justify-center">
                 <div className="ud-info-meta text-center px-4">
                   <h5 className="font-semibold p-4">Сканируйте QR-код для получения информации о культурном обмене с городом Санкт-Петербург</h5>
-                  <img src="QrEx.png" alt="QR-code A" className="w-500 h-500 md:w-auto md:h-auto mx-auto" />
+                  <img src="QrEx.png" alt="QR-code A" className="w-500 h-500 md:w-auto md:h-auto mx-auto my-4" />
                 </div>
               </div>
               {/* Второй QR-код */}
               <div className="ud-single-info w-full md:w-1/2 flex justify-center">
                 <div className="ud-info-meta text-center px-4">
                   <h5 className="font-semibold p-4">Сканируйте QR-код для получения информации о культурном обмене с городом Санкт-Петербург</h5>
-                  <img src="QrEx.png" alt="QR-code B" className="w-500 h-500 md:w-auto md:h-auto mx-auto" />
+                  <img src="QrEx.png" alt="QR-code B" className="w-500 h-500 md:w-auto md:h-auto mx-auto my-4" />
                 </div>
               </div>
             </div>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,18 +1,23 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import useOffcanvasState from '../../hooks/useOffcanvasState';
 import Offcanvas from './Offcanvas';
+import FondPages from './FondPages'; // Импортируем компонент FondPages
 
 const Header = () => {
   const { isOffcanvasOpen, toggleOffcanvas } = useOffcanvasState();
+  const [isFondPagesOpen, setIsFondPagesOpen] = useState(false); // Добавляем состояние для управления видимостью FondPages
 
-  // Используем 'useClient' для обеспечения корректной работы на стороне клиента
+  // Функция для переключения состояния видимости FondPages
+  const toggleFondPages = () => {
+    setIsFondPagesOpen(!isFondPagesOpen);
+  };
+
   const isClient = typeof window !== 'undefined';
 
-  // Проверяем, открыты ли контакты, и рендерим компонент Offcanvas, если это так
   const renderOffcanvas = () => {
     if (isOffcanvasOpen) {
-      const OffcanvasComponent = isClient ? require('./Offcanvas').default : null; // Ленивая загрузка Offcanvas
+      const OffcanvasComponent = isClient ? require('./Offcanvas').default : null;
       return OffcanvasComponent && <OffcanvasComponent />;
     }
     return null;
@@ -21,28 +26,32 @@ const Header = () => {
   return (
     <header className="bg-slate-200/75 h-16 z-10 fixed top-0 w-full">
       <nav className="mx-auto flex max-w-7xl justify-center p-2 px-8">
-      <div className=" flex gap-x-12 items-end  ">
-          <a href="#" className="text-sm ">
+        <div className="flex gap-x-12 items-end">
+          <a href="#" className="text-sm">
             Главная
           </a>
-          <a href="#" className="text-sm ">
+          <a href="#" className="text-sm">
             O нас
           </a>
-          <a href="#" className="text-sm ">
+          <a href="#" className="text-sm">
             Новости
           </a>
           <p className="text-3xl text-violet-600">Синергия</p>
-          <a href="#" className="text-sm ">
+          <a href="#" className="text-sm">
             Города-побратимы
           </a>
           <div className="flex gap-x-12 items-end">
           <button onClick={toggleOffcanvas} className="text-sm focus:outline-none">Контакты</button>
         </div>
-          <a href="#" className="text-sm ">
-            Страницы
-          </a>
+          <div className="relative inline-block">
+            {/* Добавляем кнопку для открытия/закрытия FondPages */}
+            <button onClick={toggleFondPages} className="text-sm focus:outline-none">Фонд</button>
+            {/* Рендерим компонент FondPages если isFondPagesOpen равен true */}
+            {isFondPagesOpen && <FondPages />}
+          </div>
         </div>
       </nav>
+      {/* Рендерим компонент Offcanvas */}
       {renderOffcanvas()}
     </header>
   );
