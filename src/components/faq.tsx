2d0c74e2a62571af0eb51826a08eae8265af53f0
maxim <kepl200@gmail.com>
2024-05-12 02:04:16 +0300

    faq basic done

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -9,9 +9,49 @@ const items = [
 
 const Faq: React.FC = () => {
   return (
-    <section className=" relative flex bg-black h-96 z-1 w- justify-center ">
-      <h1>Dropdown Example</h1>
-      <ExpandableText text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper justo eu eros convallis, in gravida elit rutrum. Nullam in tempor urna. Donec tristique vitae risus eget ultricies. Sed dapibus arcu et nunc dictum, vel volutpat purus consequat. Fusce id velit consequat, fringilla arcu sed, euismod nunc." />
+    <section className="  relative flex bg-white h-max z-1 w-screen justify-center ">
+      <div className="flex flex-row space-x-5 py-10">
+        <div className="flex flex-col mx-6 space-y-10">
+          <div className=" ">
+            <ExpandableText
+              text={"Что значит Синергия?"}
+              title={"Что значит Синергия?"}
+            />
+          </div>
+          <div className="  ">
+            <ExpandableText
+              text={"Сколько организаций в нашем фонде?"}
+              title={"Сколько организаций в нашем фонде?"}
+            />
+          </div>
+          <div className=" ">
+            <ExpandableText
+              text={"Конферения городов-побратимов"}
+              title={"Конферения городов-побратимов"}
+            />
+          </div>
+        </div>
+        <div className="flex flex-col pr-6 space-y-10">
+          <div className="  ">
+            <ExpandableText
+              text={"Ярмарка Дары Востока"}
+              title={"Ярмарка Дары Востока"}
+            />
+          </div>
+          <div className=" ">
+            <ExpandableText
+              text={"AUE"}
+              title={"Конференции городов-побратимов Санкт-Петербурга"}
+            />
+          </div>
+          <div className="  ">
+            <ExpandableText
+              text={"AUE"}
+              title={"Проектная деятельность фонд СИНЕРГИЯ"}
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { IconContext } from "react-icons";
+import { IoIosArrowDown } from "react-icons/io";
 
 interface ExpandableTextProps {
   text: string;
+  title: string;
 }
 
-const ExpandableText: React.FC<ExpandableTextProps> = ({ text }) => {
+const ExpandableText: React.FC<ExpandableTextProps> = ({ text, title }) => {
   const [expanded, setExpanded] = useState(false);
 
   const toggleExpand = () => {
@@ -12,15 +15,33 @@ const ExpandableText: React.FC<ExpandableTextProps> = ({ text }) => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col rounded-md border-2 max-w-md py-5 px-5 relative ">
       <button
         onClick={toggleExpand}
-        className="text-blue-500 hover:underline focus:outline-none"
+        className="  focus:outline-none  basis-1/2   inset-x-0 top-0 rounded-md "
       >
-        {expanded ? "Свернуть" : "Развернуть"}
+        {expanded ? (
+          <div className=" flex flex-row basis-5/6 ">
+            <p className="text-lg font-semibold">{title}</p>
+            <IconContext.Provider
+              value={{ className: "shared-class px-1 rotate-180", size: "25" }}
+            >
+              <IoIosArrowDown />
+            </IconContext.Provider>
+          </div>
+        ) : (
+          <div className="flex flex-row">
+            <p className="text-lg font-semibold">{title}</p>
+            <IconContext.Provider
+              value={{ className: "shared-class px-1", size: "25" }}
+            >
+              <IoIosArrowDown />
+            </IconContext.Provider>
+          </div>
+        )}
       </button>
       {expanded && (
-        <div className="mt-2">
+        <div className="mt-2 inset-x-0 top-0">
           <p className="text-gray-700">{text}</p>
         </div>
       )}
