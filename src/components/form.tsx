8174e60eea7b3e5df3aeaf8c98e34c866e8de954
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-05-27 12:00:56 +0300

    фикс

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -50,19 +50,13 @@ export default function Form() {
                   </div>
                 </div>
               </form>
-<<<<<<< Updated upstream
               <div>
                 <button
-=======
-             <div> 
-               {/* <button
-                  onClick={POST}
->>>>>>> Stashed changes
                   type="button"
                   className="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 >
                   Отправть письмо
-                </button>  */}
+                </button>  */
               </div>
             </div>
           </div>
