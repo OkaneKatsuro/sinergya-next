152295a55f099247aa0dcf60e1c6a28f07b513f8
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-06 22:44:57 +0300

    form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -4,10 +4,11 @@ import "@/components/styles/Form.scss";
 
 export default function Form() {
   return (
-    <section className=" section flex justify-center">
-      <div className=" flex flex-row z-1">
-        <div className="flex items-center mt-20">
-          <div className="hidden md:flex container w-max  rounded  flex-col ">
+    <section className="section flex justify-center">
+      <div className="flex flex-row z-1 w-full">
+        {/* Hidden on small screens */}
+        <div className="hidden md:flex items-center mt-20">
+          <div className="container w-max rounded flex-col">
             <div className="m-9 text-5xl">Свяжитесь с нами</div>
             <div className="flex flex-row m-9 pt-14 space-x-10">
               <div className="container flex flex-col">
@@ -23,25 +24,26 @@ export default function Form() {
             </div>
           </div>
         </div>
-        <div className="flex justify-start items-center py-28">
-          <div className=" bg-white drop-shadow-lg container w-max   rounded  py-8">
-            <div className="m-9 ">
+        {/* Form */}
+        <div className="flex justify-start items-center py-28 w-full">
+          <div className="bg-white drop-shadow-lg container w-max rounded py-8 mx-auto">
+            <div className="m-9">
               <form>
-                <div className="">
+                <div>
                   <h2 className="text-3xl font-semibold">
                     Отправте нам письмо
                   </h2>
                 </div>
-                <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8 ">
+                <div className="mt-3 grid grid-cols-1 gap-x-6 gap-y-8">
                   <div className="col-span-4">
                     <div className="space-y-10">
                       <Input name="Имя" placeholder="Ваше имя" />
                       <Input name="Почта" placeholder="example@yourmail.com" />
                       <Input name="Телефон" placeholder="+7(XXX)-XXX-XX-XX" />
-                      <div className=" flex justify-center items-center">
+                      <div className="flex justify-center items-center">
                         <button
                           type="button"
-                          className="  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                         >
                           Отправить письмо
                         </button>
