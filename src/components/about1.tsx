69c5c88e66770e62ff9b4427a48296d142a3807e
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-06 22:34:42 +0300

    about1

--- src/components/about1.tsx
+++ src/components/about1.tsx
@@ -15,11 +15,13 @@ export default function About1() {
     });
   }, []);
   return (
-    <section className=" relative flex  bg-white h-max z-1 py-10">
-      <div className=" mx-20  flex flex-col space-y-7">
-        <div className="flex-col w-3/5 space-y-9">
-          <p className="text-5xl font-bold  ">Мы – То, Что Мы Делаем</p>
-          <p className="text-xl pb-10 ">
+    <section className="relative flex bg-white h-max z-1 py-10">
+      <div className="container mx-auto flex flex-col space-y-7">
+        <div className="text-center w-full md:w-3/5 mx-auto space-y-9">
+          <p className="text-3xl md:text-5xl font-bold">
+            Мы – То, Что Мы Делаем
+          </p>
+          <p className="text-base md:text-xl pb-10">
             Фонд "СИНЕРГИЯ" больше, чем превосходство группы над индивидами! Это
             проектная деятельность и сверхаддитивный эффект! Это раскрытые
             таланты, насыщенная жизнь, творчество, лидерство, социальная
@@ -28,33 +30,33 @@ export default function About1() {
             коммуникация; планирование; совместная работа; синергия: 1+1=3
           </p>
         </div>
-        <div className="flex flex-row space-x-20">
-          <div className="grid ">
-            <div className="space-y-4 ">
+        <div className="flex flex-col md:flex-row md:space-x-20 space-y-10 md:space-y-0 items-center">
+          <div className="grid text-center">
+            <div className="space-y-4">
               <div className="relative">
-                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl  relative">
+                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl relative mx-auto">
                   <Image src="./planet.svg" alt="atom" width={32} height={34} />
                 </div>
-                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300"></div>
+                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300 mx-auto"></div>
               </div>
               <p className="font-bold">
                 Организует свою работу в тесном взаимодействии с органами
                 исполнительной власти
               </p>
               <p className="text-gray-400">
-                C государственными, общественными и коммерческими организациями
+                С государственными, общественными и коммерческими организациями
                 РФ, а также с учреждениями и организациями стран ЕАЭС и стран
                 БРИКС+.
               </p>
             </div>
           </div>
-          <div className="grid  ">
+          <div className="grid text-center">
             <div className="space-y-4">
               <div className="relative">
-                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl  relative">
+                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl relative mx-auto">
                   <Image src="./planet.svg" alt="atom" width={32} height={34} />
                 </div>
-                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300"></div>
+                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300 mx-auto"></div>
               </div>
               <p className="font-bold">
                 Организация культурных мероприятий и фестивалей для
@@ -66,13 +68,13 @@ export default function About1() {
               </p>
             </div>
           </div>
-          <div className="grid ">
+          <div className="grid text-center">
             <div className="space-y-4">
               <div className="relative">
-                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl  relative">
+                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl relative mx-auto">
                   <Image src="./planet.svg" alt="atom" width={32} height={34} />
                 </div>
-                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300"></div>
+                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300 mx-auto"></div>
               </div>
               <p className="font-bold">
                 Помощь пострадавшим от стихийных бедствий и кризисных ситуаций
@@ -83,13 +85,13 @@ export default function About1() {
               </p>
             </div>
           </div>
-          <div className="grid ">
+          <div className="grid text-center">
             <div className="space-y-4">
               <div className="relative">
-                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl  relative ">
+                <div className="flex justify-center items-center bg-fuchsia-700 h-16 w-16 rounded-xl relative mx-auto">
                   <Image src="./planet.svg" alt="atom" width={32} height={34} />
                 </div>
-                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300"></div>
+                <div className="w-16 h-16 bg-fuchsia-700/20 rotate-45 absolute inset-x-0 top-0 rounded-xl hover:rotate-12 transition-all duration-300 mx-auto"></div>
               </div>
               <p className="font-bold">
                 Проведение мероприятий и кампаний для укрепления позитивного
@@ -103,7 +105,7 @@ export default function About1() {
             </div>
           </div>
         </div>
-        <div className=" hidden md:image-container lg:absolute top-0 right-0 mt-8 lg:mt-12 mr-8 lg:mr-12 z-1">
+        <div className="hidden md:block lg:absolute top-0 right-0 mt-8 lg:mt-12 mr-8 lg:mr-12 z-1">
           <Image
             src="./atom.svg"
             alt="atom"
