b03ccdd843e839dc722c332016c3b3eabe2964f0
Franceslav <107437303+Franceslav@users.noreply.github.com>
2024-06-08 16:23:58 +0300

    z

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,22 +8,22 @@ export default function Form() {
       <div className="flex flex-col md:flex-row z-1 w-full">
         {/* Contact Information - Hidden on small screens */}
         <div className="hidden md:flex flex-col md:flex-row items-center md:items-start mt-20 mx-4 md:mx-0 md:ml-40 md:mr-auto">
-  <div className="container w-full md:w-max rounded flex-col">
-    <div className="m-9 text-5xl">Свяжитесь с нами</div>
-    <div className="flex flex-row m-9 md:pt-14 space-x-10">
-      <div className="container flex flex-col">
-        <p className="text-xl pb-3">Наше расположение</p>
-        <p>улица Славянская, дом 3</p>
-      </div>
-      <div className="container flex flex-col">
-        <div className="container flex flex-col">
-          <p className="text-xl pb-3">Как мы можем помочь?</p>
-          <p>fond.synergy@mail.ru</p>
+          <div className="container w-full md:w-max rounded flex-col">
+            <div className="m-9 text-5xl text-center md:text-left">Свяжитесь с нами</div>
+            <div className="flex flex-row m-9 md:pt-14 space-x-10">
+              <div className="container flex flex-col">
+                <p className="text-xl pb-3">Наше расположение</p>
+                <p>улица Славянская, дом 3</p>
+              </div>
+              <div className="container flex flex-col">
+                <div className="container flex flex-col">
+                  <p className="text-xl pb-3">Как мы можем помочь?</p>
+                  <p>fond.synergy@mail.ru</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
         {/* Form */}
         <div className="flex justify-center items-center py-28 w-full">
           <div className="bg-white drop-shadow-lg container w-max rounded py-8 mx-auto">
@@ -41,7 +41,7 @@ export default function Form() {
                       <div className="flex justify-center items-center">
                         <button
                           type="button"
-                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                          className="text-white bg-purple-600 hover:bg-purple-600 focus:ring-4 focus:ring-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-600 focus:outline-none dark:focus:ring-purple-600"
                         >
                           Отправить письмо
                         </button>
